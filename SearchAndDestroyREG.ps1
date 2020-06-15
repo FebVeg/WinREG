d313d6cd3c21e95a2333d730de4085bb384931ae
FebVeg <feb19958@gmail.com>
2020-06-15 16:59:21 +0200

    Update SearchAndDestroyREG.ps1

--- SearchAndDestroyREG.ps1
+++ SearchAndDestroyREG.ps1
@@ -1,15 +1,15 @@
-#$Reg = Read-Host "Enter Parent Key Name"
-#Get-ChildItem -Path HKCU:\ -Recurse -Include "*$reg*" | Select-Object Name # Restituisce solo il percorso della chiave
+# $Reg = Read-Host "Enter Parent Key Name"
+# Get-ChildItem -Path HKCU:\ -Recurse -Include "*$reg*" | Select-Object Name # Restituisce solo il percorso della chiave
 # just for testing.. 
 # this script can clean the registry after have uninstalled program
 
-$KeyName = Read-Host "Enter Sub Key Name: "
+$KeyName = Read-Host "Enter Sub Key Name"
 # Searching all SubKeys
 for (($i = 1); $i -lt 10; $i++)
 {
     $var = "\*"*$i
     Write-Host "Searching under HKCU:$var the subkey $KeyName"
-    Get-ItemProperty -Path HKCU:$var -Name "$KeyName" -ErrorAction SilentlyContinue
+    Get-ItemProperty -Path HKCU:$var -Name "*$KeyName*" -ErrorAction SilentlyContinue
 }
 Write-Host "Done for $KeyName"
 
