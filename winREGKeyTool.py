4375eaeba8afc52be33ecc80a596bccbae1b48e1
FebVeg <feb19958@gmail.com>
2020-06-18 14:20:49 +0200

    Update winREGKeyTool.py

--- winREGKeyTool.py
+++ winREGKeyTool.py
@@ -1,20 +1,12 @@
-try:
-    import os
-    import subprocess
-    import sys
-    import time
-    import re
-    import threading
-except Exception as error:
-    print("Library error: " + error)
+import os
+import subprocess
+import sys
+import time
+import re
+import itertools
+import threading
 
 os.system("cls") # pulisco CMD
-
-print("Register Key Finder & Destroyer\n")
-
-if os.name == "posix":
-    print("This script is for Windows System")
-    sys.exit()
 
 username = os.getlogin() # Username Info
 workDir = os.path.dirname(os.path.abspath(__file__)) + "\\" # WorkDire Info 
@@ -25,107 +17,141 @@ print("[i] Username: " + username)
 print("[i] Target: " + hostname)
 print("[i] Working Directory: " + workDir)
 
-searchKeys = input('\n[?] Type a string that you want search: ')
-searchSoftware = input("[?] Need to search from another dir (Es. /software)? [y/n]: ")
+searchKeys = input('\n[-] Type a string that you want search: ')
+searchSoftware = input("[-] Need to search a program? [y/n]: ")
 
 outputKeysNameHKLM = "regKeysHKLM_" + searchKeys + ".txt"
 outputKeysNameHKCU = "regKeysHKCU_" + searchKeys + ".txt"
 
 if searchSoftware == "y":
-    searchSoftware = input("[?] Dir: ")
-    searchSoftware = "\\"+searchSoftware
+    searchSoftware = "\\Software"
 elif searchSoftware == "n":
     searchSoftware = ""
 
 if os.path.exists(workDir + outputKeysNameHKLM):
     print("[!] File %s already existing" % (outputKeysNameHKLM))
+    sys.exit()
+
+if os.path.exists(workDir + outputKeysNameHKCU):
     print("[!] File %s already existing" % (outputKeysNameHKCU))
     sys.exit()
 
-print("\n[i] CMD output File: " + workDir + outputKeysNameHKLM.replace('"', ''))
-print("[i] CMD output File: " + workDir + outputKeysNameHKCU.replace('"', ''))
+print("[i] Output file: " + workDir + outputKeysNameHKLM.replace('"', ''))
+print("[i] Output file: " + workDir + outputKeysNameHKCU.replace('"', ''))
 
-# Genero il comando CMD
-cmdHKLM = 'REG Query HKLM%s /F %s /S > %s%s' % (searchSoftware, searchKeys, workDir, outputKeysNameHKLM)
-cmdHKCU = 'REG Query HKCU%s /F %s /S > %s%s' % (searchSoftware, searchKeys, workDir, outputKeysNameHKCU)
-print("\n[CMD] " + cmdHKLM + "\n[CMD] " + cmdHKCU)
+def searching_function():
+    # Genero il comando CMD
+    cmdHKLM = 'REG Query HKLM%s /F %s /S > %s%s' % (searchSoftware, searchKeys, workDir, outputKeysNameHKLM)
+    cmdHKCU = 'REG Query HKCU%s /F %s /S > %s%s' % (searchSoftware, searchKeys, workDir, outputKeysNameHKCU)
 
-# Inizio comando CMD
-try: 
-    print("\n[+] Searching '%s' on HKLM.... Wait...." % (searchKeys))
-    os.system(cmdHKLM)
-    print("[+] Searching '%s' on HKCU.... Wait...." % (searchKeys))
-    os.system(cmdHKCU)
-    print("[+] Searching finished\n")
-except Exception as error:
-    print("Errore comando: " + error)
-    sys.exit()
-# Fine comando CMD
+    # Inizio comando CMD
+    try: 
+        os.system(cmdHKLM)
+        os.system(cmdHKCU)
+    except SystemError as error:
+        print(error)  
+    # Fine comando CMD
+
+done = False
+def animate():
+    print("-"*50)
+    #for c in itertools.cycle(['|', '/', '-', '\\']):
+    for c in itertools.cycle(['[i] Searching...',
+                              '[i] sEarching...',
+                              '[i] seArching...',
+                              '[i] seaRching...',
+                              '[i] searChing...',
+                              '[i] searcHing...',
+                              '[i] searchIng...',
+                              '[i] searchiNg...',
+                              '[i] searchinG...',
+                              '[i] searchiNg...',
+                              '[i] searchIng...',
+                              '[i] searcHing...',
+                              '[i] searChing...',
+                              '[i] seaRching...',
+                              '[i] seArching...',
+                              '[i] sEarching...',
+                              ]):
+        if done:
+            break
+        sys.stdout.write('\r' + c)
+        sys.stdout.flush()
+        time.sleep(0.1)
+    sys.stdout.write('\rDone            ')
+
+t = threading.Thread(target=animate)
+t.start()
+
+searching_function()
+time.sleep(1)
+done = True
 
 # Edito il file di output e lo riscrivo senza nuove linee e spazi
 outputKeysNameHKCU = outputKeysNameHKCU.replace('"', '')
 outputKeysNameHKLM = outputKeysNameHKLM.replace('"', '')
 
 try:
-    if os.path.isfile(workDir + outputKeysNameHKLM):
-        output_file_remastered = os.path.splitext(os.path.basename(outputKeysNameHKLM))[0] + "_mod.txt"
-        keys = open(workDir + outputKeysNameHKLM).readlines()
-        for line in keys:
-            if line.startswith('HK'):
-                line = line.strip()
-                line = re.sub(r' \s.*$', '', line)
-                string = line.replace("\n", "")
-                #print("[!] Key found: " + str(string))
-                new_file = open(workDir + output_file_remastered, "a+")
-                new_file.write(str(line) + "\n")
-                new_file.close()
-            else:
-                continue
+    output_file_remastered = os.path.splitext(os.path.basename(outputKeysNameHKLM))[0] + "_mod.txt"
+    keys = open(workDir + outputKeysNameHKLM).readlines()
+    for line in keys:
+        if line.startswith('HK'):
+            line = line.strip()
+            line = re.sub(r' \s.*$', '', line)
+            string = line.replace("\n", "")
+            print("[!] Key found: " + str(string))
+            new_file = open(workDir + output_file_remastered, "a+")
+            new_file.write(str(line) + "\n")
+        else:
+            continue
 
-            if line.startswith('    HK'):
-                line = line.strip()
-                line = re.sub(r' \s.*$', '', line)
-                string = line.replace("\n", "")
-                #print("[!] Key found: " + str(string))
-                new_file = open(workDir + output_file_remastered, "a+")
-                new_file.write(str(line) + "\n")
-                new_file.close()
-            else:
-                continue
-    if os.path.exists(workDir + output_file_remastered):
-        print("Check the output: " + output_file_remastered)
+        if line.startswith('    HK'):
+            line = line.strip()
+            line = re.sub(r' \s.*$', '', line)
+            string = line.replace("\n", "")
+            print("[!] Key found: " + str(string))
+            new_file = open(workDir + output_file_remastered, "a+")
+            new_file.write(str(line) + "\n")
+        else:
+            continue
+    a1 = "[i] New output file: " + output_file_remastered
 except Exception as error:
     print(error)
 
 try:
-    if os.path.isfile(workDir + outputKeysNameHKCU):
-        output_file_remastered = os.path.splitext(os.path.basename(outputKeysNameHKCU))[0] + "_mod.txt"
-        keys = open(workDir + outputKeysNameHKCU).readlines()
-        for line in keys:
-            if line.startswith('HK'):
-                line = line.strip()
-                line = re.sub(r' \s.*$', '', line)
-                string = line.replace("\n", "")
-                #print("[!] Key found: " + str(string))
-                new_file = open(workDir + output_file_remastered, "a+")
-                new_file.write(str(line) + "\n")
-                new_file.close()
-            else:
-                continue
+    output_file_remastered = os.path.splitext(os.path.basename(outputKeysNameHKCU))[0] + "_mod.txt"
+    keys = open(workDir + outputKeysNameHKCU).readlines()
+    for line in keys:
+        if line.startswith('HK'):
+            line = line.strip()
+            line = re.sub(r' \s.*$', '', line)
+            string = line.replace("\n", "")
+            print("[!] Key found: " + str(string))
+            new_file = open(workDir + output_file_remastered, "a+")
+            new_file.write(str(line) + "\n")
+        else:
+            continue
 
-            if line.startswith('    HK'):
-                line = line.strip()
-                line = re.sub(r' \s.*$', '', line)
-                string = line.replace("\n", "")
-                #print("[!] Key found: " + str(string))
-                new_file = open(workDir + output_file_remastered, "a+")
-                new_file.write(str(line) + "\n")
-                new_file.close()
-            else:
-                continue
-    if os.path.exists(workDir + output_file_remastered):
-        print("Check the output: " + output_file_remastered)
+        if line.startswith('    HK'):
+            line = line.strip()
+            line = re.sub(r' \s.*$', '', line)
+            string = line.replace("\n", "")
+            print("[!] Key found: " + str(string))
+            new_file = open(workDir + output_file_remastered, "a+")
+            new_file.write(str(line) + "\n")
+        else:
+            continue
+        
+    a2 = "[i] New output file: " + output_file_remastered
 
-    print("\n[-] All processes are done for: " + searchKeys)
+    try:
+        new_file.close()
+        if os.path.isfile(workDir + a1) and os.path.isfile(workDir + a2):
+            print("[i] Output saved: " + a1)
+            print("[i] Output saved: " + a2)
+    except Exception as error:
+        print("[i] Nothing found, nothing to save")
+    
+    print("[-] Research finished for: " + searchKeys)
 except Exception as error:
-    print(error)    
+    print(error)
